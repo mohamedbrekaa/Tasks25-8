996e4a865eca41c83a5cbcfd1b54ea6bd418b127
mohamedbrekaa <mohamedbrekaa222@gmail.com>
2023-08-31 19:13:01 +0300

    first commit

---
+++ code.js
@@ -0,0 +1,52 @@
+// var text = +"10"
+// console.log(typeof(text))
+// var candies = {
+//     chocolate: 10,
+//     lollipops: null,
+//     sourStrips: undefined,
+//     jellyBeans: 8,
+//     licorice: null
+
+// }
+// var Hi = "gummyBears";
+// console.log(candies.lollipops ?? Hi );
+// new task
+// var Restaurant = {
+//     meat: 2,
+//     chick: 3,
+// }
+// function Mohamed (name, age){
+//     if (age >= 20){
+//         console.log(`Age: ${age} : ${"sandwich.meat"}`);
+//     } else {
+//         console.log(`Age: ${age} : ${"sandwich.chicken"}`);
+//     }
+// }
+// Mohamed("moody", 20)
+// Mohamed("Ali", 18)
+
+// new task
+
+function calculate (num1, num2, method){
+    if (method === "sum"){
+        var result = num1 + num2;
+        console.log(result);
+    } else if (method === "sub"){
+        var result2 = num1 - num2;
+        console.log(result2);
+    } else if (method === "multi"){
+        var result3 = num1 * num2;
+        console.log(result3);
+    } else if (method === "division"){
+        var result4 = num1 / num2;
+        console.log(result4);
+    } else {
+        console.log("Unknown Method, Try Again");
+    }
+}
+
+calculate (1,2, "sum")
+calculate (1,2, "sub")
+calculate (1,2, "multi")
+calculate (1,2, "division")
+calculate (1,2, "frhygrhf")
